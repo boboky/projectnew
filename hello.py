1a3b80e73a7d4b26c1f4c1282f466a2ffa198917
Ola Akinyemi <ola.akinyemi@hotmail.com>
2017-03-30 23:30:28 +0100

    I just tested this if it will work

---
+++ hello.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+#def is to define the entire code in the parantesis as one code and called with the name "def"
+
+#def hello():
+#	print ("Hello World")
+#	myName= input("What is your name?")
+#		print(myName)
+	
+#	if(myName == "biola"):
+#		print("biola is the greatest")
+#	elif(myName == "bob"):
+#		print(myName + ' is cool')
+#	else:
+#		print(myName + ' is ok')
+
+#hello()
+
+#the code above can also be ran as the one below with the 'myString' to be picked from input
+def hello(myString):
+	print (myString)
+	myName= input("What is your name?")
+		#print(myName)
+	
+	if(myName == "biola"):
+		print("biola is the greatest")
+	elif(myName == "bob"):
+		print(myName + ' is cool')
+	else:
+		print(myName + ' is ok')
+
+hello("class")
+
